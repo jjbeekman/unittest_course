b827d827a46d6cea7c7fe1486a3f6b8013266340
jjbeekman <jonathanjeroen@gmail.com>
2021-12-08 17:32:21 +0100

    lecture 8

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, String, DateTime, func
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime, func, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
@@ -22,3 +22,15 @@ class Customer(Base):
     last_name = Column(String, nullable=False)
     address = Column(String, nullable=False)
     phone_number = Column(String, nullable=False)
+
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    id = Column(Integer, primary_key=True, comment="Primary-key column is auto-incremented")
+    created_at = Column(DateTime(True), nullable=False, server_default=func.now())
+
+    name = Column(String, nullable=False)
+    shop_id = Column(ForeignKey("shops.id"), nullable=False, index=True)
+
+    shop = relationship(Shop.__name__)
