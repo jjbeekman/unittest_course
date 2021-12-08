d70fb6c5b2dd649b87f3efc83975ff6ae99addb0
jjbeekman <jonathanjeroen@gmail.com>
2021-12-08 17:53:31 +0100

    lecture 9

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, DateTime, func, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, func, ForeignKey, UniqueConstraint, Boolean, text, \
+    CheckConstraint
 from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
@@ -14,6 +15,7 @@ class Shop(Base):
 
 class Customer(Base):
     __tablename__ = 'customers'
+    __table_args__ = (UniqueConstraint("phone_number"),)
 
     id = Column(Integer, primary_key=True, comment="Primary-key column is auto-incremented")
     created_at = Column(DateTime(True), nullable=False, server_default=func.now())
@@ -26,6 +28,7 @@ class Customer(Base):
 
 class Product(Base):
     __tablename__ = 'products'
+    __table_args__ = (UniqueConstraint("shop_id", "name"),)
 
     id = Column(Integer, primary_key=True, comment="Primary-key column is auto-incremented")
     created_at = Column(DateTime(True), nullable=False, server_default=func.now())
@@ -34,3 +37,20 @@ class Product(Base):
     shop_id = Column(ForeignKey("shops.id"), nullable=False, index=True)
 
     shop = relationship(Shop.__name__)
+
+
+class Order(Base):
+    __tablename__ = 'orders'
+    __table_args__ = (CheckConstraint("ordered_at < sla_at"),)
+
+    id = Column(Integer, primary_key=True, comment="Primary-key column is auto-incremented")
+    created_at = Column(DateTime(True), nullable=False, server_default=func.now())
+
+    ordered_at = Column(DateTime(True), nullable=False)
+    sla_at = Column(DateTime(True), nullable=False)  # Service-level agreement on last delivery date
+    is_fulfilled = Column(Boolean, nullable=False, server_default=text("false"))
+    product_id = Column(ForeignKey("products.id"), nullable=False, index=True)
+    customer_id = Column(ForeignKey("customers.id"), nullable=False, index=True)
+
+    product = relationship(Product.__name__)
+    customer = relationship(Customer.__name__)
