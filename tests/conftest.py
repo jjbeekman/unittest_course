6be4997f18a41d220e4e061d9428a16f74ed3fa7
jjbeekman <jonathanjeroen@gmail.com>
2021-12-11 19:05:09 +0100

    lecture 14

---
+++ tests/conftest.py
@@ -0,0 +1,46 @@
+# Anything that is defined here, will be run during the test discovery fase of pytest
+from models import Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+import pytest
+
+
+@pytest.fixture
+def connection():
+    # Setup
+    print("Creating engine")
+    engine = create_engine("sqlite:///test.db")
+    connection = engine.connect()
+    yield connection
+
+    # Teardown
+    print("Closing connection")
+    connection.close()
+
+
+@pytest.fixture
+def setup_database(connection):
+    # Setup
+    print("Creating tables")
+    Base.metadata.bind = connection
+    Base.metadata.create_all()
+    yield
+
+    # Teardown
+    print("Dropping tables")
+    Base.metadata.drop_all()
+
+
+@pytest.fixture
+def db_session(connection, setup_database):
+    # Setup
+    print("Starting ORM session")
+    scoped_db_session = scoped_session(sessionmaker())
+    scoped_db_session.configure(bind=connection)
+    db_session = scoped_db_session()
+    yield db_session
+
+    # Teardown
+    print()
+    print("Removing scoped db session")
+    scoped_db_session.remove()
--- tests/test_db_session.py
+++ tests/test_db_session.py
@@ -1,32 +1,5 @@
-from models import Base, Shop
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from models import Shop
 import pytest
-
-
-@pytest.fixture
-def db_session():
-    # Setup
-    print("Creating engine")
-    engine = create_engine("sqlite:///test.db")
-    connection = engine.connect()
-    print("Creating tables")
-    Base.metadata.bind = connection
-    Base.metadata.create_all(engine)
-    print("Starting ORM session")
-    scoped_db_session = scoped_session(sessionmaker())
-    scoped_db_session.configure(bind=connection)
-    db_session = scoped_db_session()
-    yield db_session
-
-    # Teardown
-    print()
-    print("Removing scoped db session")
-    scoped_db_session.remove()
-    print("Dropping tables")
-    Base.metadata.drop_all()
-    print("Closing connection")
-    connection.close()
 
 
 @pytest.mark.parametrize("run", (1, 2, 3))
